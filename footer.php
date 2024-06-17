e1980f6ed1366e1ad6156efa305ba9f5889bc9ec
Aniket7726 <aniketsharma.sh17@gmail.com>
2024-06-17 14:42:33 +0530

    Knee Replacement Page Done From My SIde Without Header , i Wil Create Header Later For Sure

---
+++ footer.php
@@ -0,0 +1,52 @@
+<div class="foot">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="d-flex gap-1">
+                            <img src="Images/foot-logo-1.png" alt="">
+                            <img src="Images/foot-logo-2.png" alt="">
+                        </div>
+                        <h3 class="pb-4">NAV Imperial Hospital and Research Centre is one of the leading orthopedic hospital in Jaipur, Rajasthan</h3>
+                        <button>Reach Us</button>
+                        <h6><img src="Images/location.png" alt="">Nav Imperial Hospital, Kawatiya Circle Shastri Nagar, Jaipur (India)</h6>
+                        <h6><img src="Images/call.png" alt="">+91-7891010000</h6>
+                        <p>coo@navimperialhospital.com</p>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row">
+                            <div class="col-5">
+                                <h4>LINKS</h4>
+                                <li><a href="">Home</a></li>
+                                <li><a href="">About</a></li>
+                                <li><a href="">Doctors</a></li>
+                                <li><a href="">Blog</a></li>
+                                <li><a href="">Contact</a></li>
+                            </div>
+                            <div class="col-7">
+                                <h4>Services</h4>
+                                <li><a href="">Joint Replacement</a></li>
+                                <li><a href="">Knee Replacement</a></li>
+                                <li><a href="">Bariatric Surgery</a></li>
+                                <li><a href="">General Surgery</a></li>
+                                <li><a href="">Obstetrics & Gynaecology</a></li>
+                                <li><a href="">Humanoid Robotics Surgery</a></li>
+                                <li><a href="">Urology</a></li>
+                                <li><a href="">Intensive care/ Critical Care</a></li>
+                                <li><a href="">Gastroenterology</a></li>
+                                <li><a href="">Neurology/Neurosurgery</a></li>
+                                <li><a href="">ENT</a></li>
+                                <li><a href="">Paediatrics</a></li>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <h4>Our Location</h4>
+                        <img src="Images/foot-map.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="sub-foot">
+            <h6>Copyright © 2024 Nav Imperial Hospital | All Rights Reserved | Powered by Digital Marketing Company in Jaipur.</h6>
+        </div>
