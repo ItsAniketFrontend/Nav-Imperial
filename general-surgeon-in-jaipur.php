5dceb159cbb83a0904bb1db70a43f23ccc210415
Aniket7726 <aniketsharma.sh17@gmail.com>
2024-08-10 18:10:43 +0530

    Meta Title and Meta Description added to these Pages

--- general-surgeon-in-jaipur.php
+++ general-surgeon-in-jaipur.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Experienced General Surgeon in Jaipur - Nav Imperial Hospital</title>
+    <meta name="description"
+        content="Find a skilled general surgeon in Jaipur at Navi Imperial Hospital. Offering expert surgical care and personalized treatment options for optimal patient outcomes.">
     <link rel="stylesheet" href="CSS/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
