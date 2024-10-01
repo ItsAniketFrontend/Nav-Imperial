ddc16401b7f4e8f1e15f7dc4a4a2ce488b02dd4f
Aniket7726 <aniketsharma.sh17@gmail.com>
2024-10-01 12:23:24 +0530

    New Blog Done

--- What-is-paralysis-attack-and-what-are-its-treatment-methods.php
+++ What-is-paralysis-attack-and-what-are-its-treatment-methods.php
@@ -32,73 +32,68 @@
                     <div class="col-12">
                         <h1>Paralysis: Symptoms, causes, diagnosis, and Treatment</h1>
                         <div class="blog-content-13"></div>
-                        <h3><a href="https://navimperialhospital.com/blog/multiple-myeloma-symptoms-causes-treatment.php#">Multiple Myeloma</a></h3>
-                        <p>Multiple myeloma is a type of blood cancer and it is also known as Kahler's disease. Cancerous cells in the white blood cells called plasma cells cause multiple myeloma. A healthy plasma cell makes antibodies that attack germs and help you fight infections.</p>
-                        <p>Multiple myeloma occurs when cancerous plasma cells accumulate in the bone marrow and crowd out healthy blood cells. Rather than producing antibodies that can protect the body, cancer cells produce proteins that may cause complications</p>
-                        <p>Our body makes antibodies with the help of white blood cells called plasma cells. In the case of multiple myeloma, these cells multiply incorrectly. Because of the wrong multiplication of cells, our bones and blood are flooded with too much protein (called immunoglobulin). As a result, our organs are damaged.</p>
-                        <h2><a href="https://navimperialhospital.com/blog/multiple-myeloma-symptoms-causes-treatment.php#">Signs & symptoms of multiple myeloma</a></h2>
-                        <p>If you are suffering from the following signs & symptoms of multiple myeloma, you are at risk of this disease if:</p>
-                        <li>Your back or ribs are sore or are weakened; you may have the risk of multiple myeloma.</li>
-                        <li>Your bones fracture easily</li>
-                        <li>You suffer from bone pain</li>
-                        <li>You experience fatigue and weakness</li>
-                        <li>You experience shortness of breath</li>
-                        <li>You feel dizziness</li>
-                        <li>You lose weight that is not usual</li>
-                        <li>You suffer from fever and frequent infections</li>
-                        <li>You need to urinate more frequently</li>
-                        <li>You feel extreme thirst</li>
-                        <h2><a href="https://navimperialhospital.com/blog/multiple-myeloma-symptoms-causes-treatment.php#">Causes of Multiple Myeloma</a></h2>
-                        <p>Multiple myeloma is caused by many factors. The following are the factors that cause multiple myeloma.</p>
-                        <h3>Getting older</h3>
-                        <p>A person's risk of multiple myeloma increases with age. In most cases, multiple myeloma is diagnosed in the middle years of your life.</p>
-                        <h3>Males</h3>
-                        <p>The disease is more likely to affect men than women.</p>
-                        <h3>Black race people</h3>
-                        <p>People of other races are less likely to develop multiple myeloma than black people.</p>
-                        <h3>Family history of multiple myeloma or heredity</h3>
-                        <p>You are at a higher risk of developing the disease if your brother, sister, or parent has multiple myeloma.</p>
-                        <h3>Your medical history includes monoclonal gammopathy of undetermined significance (MGUS)</h3>
-                        <p>The risk for multiple myeloma increases, if you have monoclonal gammopathy of undetermined significance condition. Multiple myeloma almost always starts as MGUS.</p>
-                        <h2>Complications in multiple myeloma</h2>
-                        <p>There are several complications associated with multiple myeloma, including:</p>
-                        <h3>Frequent infections</h3>
-                        <p>Your body's ability to fight infections is impaired by myeloma cells.</p>
-                        <h3>Bone problems</h3>
-                        <p>Bone pain, thinning bones, and broken bones can also be caused by multiple myeloma.</p>
-                        <h3>Reduced kidney function</h3>
-                        <p>There is a risk of kidney failure in patients with multiple myeloma.</p>
-                        <h3>Low red blood cell count (anemia)</h3>
-                        <p>Multiple myeloma can also cause anemia and other blood problems because myeloma cells crowd out normal blood cells.
-                        Also Read: <a href="https://navimperialhospital.com/blog/symptoms-of-vitamin-D-deficiency.php">Vitamin D: Benefits, deficiency, sources, treatment, and dosage</a></p>
-                        <h2>Diagnosis for Multiple myeloma</h2>
-                        <p>An array of tests will be conducted by your doctor to diagnose multiple myeloma. The following are the tests that your doctor will ask for</p>
-                        <h3>Blood tests-</h3>
-                        <p>Blood tests include- Beta2 microglobulin, Complete blood count, Immunofixation electrophoresis, Antibody/immunoglobulin levels, and types, Serum-free light chain assay, and Serum protein electrophoresis</p>
-                        <h3>Urine tests-</h3>
-                        <p>Urine tests include urinalysis, urine protein electrophoresis, and urine protein level</p>
-                        <h3>Bone and bone marrow tests-</h3>
-                        <p>Bone and bone marrow tests include Imaging studies, Karyotyping and fluorescence in situ hybridization (FISH), and Bone marrow biopsy or aspiration.</p>
-                        <h3>Treatment for multiple myeloma</h3>
-                        <p>A variety of treatments are available to manage the symptoms of multiple myeloma and its complications. The following treatments are available for multiple myeloma:</p>
-                        <h3>1. Proteasome inhibitors:</h3>
-                        <p>The proteasome is a complex protein that helps remove old proteins in cells so new ones can be synthesized. A proteasome inhibitor prevents cancer cells from doing that. Cells infected with cancer die as old proteins pile up in their bodies.</p>
-                        <h3>2. Immunomodulatory drugs:</h3>
-                        <p>Multiple myeloma patients rely on these drugs to manage their illnesses. Your immune system is strengthened by them. Certain immune cells turn on myeloma-killing agents, while others block cancer cell growth by killing cancer cells.</p>
-                        <h3>3. HDAC inhibitors:</h3>
-                        <p>They inhibit the production of the histone deacetylase (HDAC) protein, which helps malignant cells grow and divide rapidly.</p>
-                        <h3>4. Steroids:</h3>
-                        <p>Throughout the course of the disease, these drugs are used. Cells of multiple myeloma can be killed by high doses of steroids. Additionally, they are used to ease symptoms such as pain and pressure by preventing white blood cells from racing to the area. They can also ease nausea and vomiting caused by chemotherapy.</p>
-                        <h3>5. Chemotherapy:</h3>
-                        <p>To treat cancer, these drugs kill cells that are in the process of dividing. Also, they cause unwanted side effects by killing healthy cells around them.</p>
-                        <h3>6. Monoclonal antibodies:</h3>
-                        <p>Your immune system will be able to fight cancer with the help of these immunotherapy drugs. Your body receives antibodies that target specific proteins on multiple myeloma cells.</p>
-                        <h3>7. Radiation:</h3>
-                        <p>Radiation technology can also be used to treat multiple myeloma. High-energy particles, or rays, are used to destroy cancer cells and prevent them from growing.</p>
-                        <h3>8. Stem cell transplants:</h3>
-                        <p>Multiple myeloma can be managed with two stem cell transplants. There are two kinds of stem cell transplant for multiple myeloma and that includes the following</p>
-                        <h3>Autologous stem cell transplants- The stem cells from your own body are used for autologous transplants.<br>
-                        Allogeneic stem cell transplants – In allogeneic stem cell transplants, donor cells are used.</h3>
+                        <p>When a portion of your body does not have muscle movement, it is considered paralyzed. Paralysis can affect any part of your body at any time in your life. A person who is paralyzed lacks the ability to control a muscle or to command the muscles gathered together in one piece of their body. It is almost certainly due to a problem among the nerve cells that connect the body part to the brain and back again. Nerve cells transmit the signals that direct the movement of your muscles.</p>
+                        <p>A person can be born with paralysis due to a birth deformity, such as spina bifida, where the brain, spinal cord, or perhaps the covering that encloses them don't form in the correct way. Paralysis can be short-term or long-term, transitory or permanent, restricted or cumulative.
+                        Based on the underlying cause of paralysis, as well as the symptoms experienced by the patient, a treatment plan will be decided. Adapting to technological advances and making remedial interventions may help patients maintain their freedom and personal happiness.</p>
+                        <h2><a href="https://navimperialhospital.com/blog/What-is-paralysis-attack-and-what-are-its-treatment-methods.php#">Symptoms of Paralysis</a></h2>
+                        <p>Cerebral trigger signs act as a constraint for muscle development. Any harm to the transfer framework, for instance, to the brain, spinal cord, nerves, or the intersection between the nerve and muscles, will result in paralysis symptoms. Although the symptoms of paralysis may vary depending on the cause, they are usually easy to spot. The following are the symptoms of paralysis</p>
+                        <li>Issues with blood flow, breathing, and pulse</li>
+                        <li>Blood clots in the legs</li>
+                        <li>Temperament and behaviour change</li>
+                        <li>Organ, gland, and tissue dysfunction</li>
+                        <li>Speaking or gulping issues</li>
+                        <li>Pressing factor injuries and skin injuries</li>
+                        <li>Constipation and loss of urine control</li>
+                        <h2><a href="https://navimperialhospital.com/blog/What-is-paralysis-attack-and-what-are-its-treatment-methods.php#">Causes of Paralysis</a></h2>
+                        <p>Paralysis is a condition in which muscles are paralyzed. It is important to realize that when a part of the transmission framework like the brain, spinal band, nerves, or the nerve-muscle connection is damaged, the signal to move will not endure to the muscles, which will cause paralysis. Paralysis can be caused by many factors, but is typically the result of a stroke, usually from a blocked artery in your neck or brain. The following are some other possible causes of paralysis-</p>
+                        <li>Poliomyelitis</li>
+                        <li>Parkinson's disease</li>
+                        <li>Cerebral palsy</li>
+                        <li>Multiple sclerosis</li>
+                        <li>Nerve injury</li>
+                        <li>ALS</li>
+                        <li>Spinal cord injury</li>
+                        <li>Peripheral neuropathy</li>
+                        <li>Guillain–Barré syndrome</li>
+                        <h2>Types of paralysis</h2>
+                        <p>The following are the types and levels of paralysis.</p>
+                        <h2>Partial paralysis</h2>
+                        <p>In partial or deficient paralysis, you actually make the dead muscle feel as though it is alive, giving you some control over it. Sometimes this is referred to as paresis.</p>
+                        <h2>Complete paralysis</h2>
+                        <p>A complete paralysis occurs when your muscles are deadened to the point where you can't move them or control them at all. Additionally, those muscles will no longer feel anything.</p>
+                        <h2>Locked-in syndrome</h2>
+                        <p>The most uncommon and most serious form of paralysis is a locked-in syndrome, where the affected individual is not able to control their entire range of muscles, except those that control their eyes.</p>
+                        <h2>Localized paralysis</h2>
+                        <p>In localized paralysis, only one specific part of the body is affected, like your face, hands, or feet.</p>
+                        <h2>Monoplegia</h2>
+                        <p>Monoplegia is a type of summed-up paralysis that affects only one appendage.</p>
+                        <h2>Diplegia</h2>
+                        <p>A diplegic person experiences similar circumstances on either side of the body, such as affecting both sides of the arms, the legs, and both sides of the face.</p>
+                        <h2>Quadriplegia</h2>
+                        <p>Quadriplegia (or tetraplegia) is a condition where all the appendages, sometimes along with specific organs, are paralyzed.</p>
+                        <h2>Paraplegia</h2>
+                        <p>The paralysis of the waist is called paraplegia.</p>
+                        <h2>Generalized paralysis</h2>
+                        <p>Generalized paralysis affects a larger part of your body and is more widespread due to the extent to which it is impacted.
+                        Also Read: <a href="https://navimperialhospital.com/blog/sports-injury.php">Sports Injury: Types, Treatment, Exercises, and Prevention</a></p>
+                        <h2>Diagnosis of Paralysis</h2>
+                        <p>In most cases, it will be evident if the paralysis is caused by an event like a stroke or spinal cord injury. In most cases, paralysis is easy to diagnose since the primary side effect of paralysis is self-evident. Identifying the reason for the paralysis is an important part. Depending on the injury, the specialist may need to learn more about the degree of paralysis that is occurring, and the condition of the nerves involved. The following tests may be recommended by doctors to accomplish that-</p>
+                        <h3>CT Scan</h3>
+                        <p>The CT scan uses PCs to combine X-ray images into cross-sectional views, which show the body as a whole.</p>
+                        <h3>X-ray</h3>
+                        <p>Using modest amounts of radiation, this test delivers a close-up view of dense structures within the body, like the bones.</p>
+                        <h3>MRI</h3>
+                        <p>A powerful magnet, radio waves, and a PC are used in MRIs to provide accurate images of the body.</p>
+                        <h3>Electromyography (EMG)</h3>
+                        <p>The purpose of this test is to measure electrical movement in nerves and muscle</p>
+                        <h3>Myelography</h3>
+                        <p>To perform this test, a differentiation color is infused into a spinal channel to boost the visibility of nerves on an X-ray, MRI, or CT scan.</p>
+                        <h3>Spinal tap</h3>
+                        <p>A long needle is inserted into the spine to collect spinal fluid.</p>
+                        <h2>Treatment for paralysis</h2>
+                        <p>The treatment options for paralysis are as follows</p>
+                        <li>Physical therapy</li>
+                        <li>Occupational therapy</li>
                     </div>
                 </div>
             </div>
